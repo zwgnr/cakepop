7ecfc22a0c94c6f3c370f29176a5a6e7703182e6
zwagnr <zachwagner@hey.com>
2023-02-14 17:13:23 -0500

    improve docs

--- apps/docs/components/button.tsx
+++
@@ -1,6 +0,0 @@
-import '@cakepop/react/styles';
-import { Button, defaultTheme } from '@cakepop/react';
-
-export const CPButton = () => {
-  return <Button className={defaultTheme}>Button</Button>;
-};
--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>CakepopUI</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/zwagnr/cakepop',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  //chat: {
+  //  link: 'https://discord.com',
+ // },
+  docsRepositoryBase: 'https://github.com/zwagnr/cakepop',
   footer: {
     text: 'CakepopUI',
   },
